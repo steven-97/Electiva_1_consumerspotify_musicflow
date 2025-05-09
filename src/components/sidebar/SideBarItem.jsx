18da6c023886e153859e5069fc77420464414121
steven-97 <101261150+steven-97@users.noreply.github.com>
2025-05-09 16:56:32 -0500

    Primer commit

--- src/components/sidebar/SideBarItem.jsx
+++ src/components/sidebar/SideBarItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SidebarItem = ({ title, subtitle, image}) => (
-  <li className="bg-[#1A1A35] px-3 py-2 rounded-lg hover:bg-[#2A2A4B] transition-all flex items-center gap-3">
+  <li className="bg-[#1A1A35] px-3 py-2 mt-1 rounded-lg hover:bg-[#2A2A4B] transition-all flex items-center gap-3">
     <img
       src={image}
       alt="Album cover"
